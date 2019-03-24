beede1b761a8326b74487c657bce9c84359dcdad
ravi4rails <ravionrails@gmail.com>
2019-03-24 23:36:42 +0530

    added methods for devise redirections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
 
   private
 
+    def after_sign_in_path_for(resource)
+      todos_path
+    end
+
+    def after_sign_up_path_for(resource)
+      todos_path
+    end
+
+    def after_sign_out_path_for(resource)
+      root_path
+    end
+
     def layout_by_resource
       if devise_controller?
         "devise"
